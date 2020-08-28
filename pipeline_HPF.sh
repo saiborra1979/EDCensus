6d73f8d2cf5f913fdcb1a66195f63212fe50b793
ErikinBC <alla_hack@hotmail.com>
2020-08-28 13:40:34 -0400

    fixing HPF

--- pipeline_HPF.sh
+++ pipeline_HPF.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#PBS -l walltime=01:00:00
+#PBS -l walltime=06:00:00
 #PBS -o /home/edrysdale/qsub/
 #PBS -e /home/edrysdale/qsub/
 #PBS -l vmem=16g
 #PBS -l mem=16g
+#PBS -l nodes=1:ppn=4
+#PBS -t 1-12
 
-# Assign the lead
-lead=$(($1))
-model=$2
-echo "Lead: "$lead", model: "$model
+# Note that the array is over the lead
 
 # EXAMPLE OF HOW TO RUN
-#qsub -N local_lead4_lasso -F "4 lasso" -t 0-181 -l nodes=1:ppn=1 pipeline_HPF.sh
+#qsub -N gpy_run pipeline_HPF.sh
+#-F "4 lasso"  -t 0-181  -l nodes=1:ppn=1
 
 pwd
 cd /hpf/largeprojects/agoldenb/edrysdale/ED/CensusFlow
@@ -21,23 +21,14 @@ pwd
 source activate CensusFlow
 which python
 
-# (1) Remove any e/o files not associated with this run
 echo "JOBID = "$PBS_JOBID
-#jobid=$(echo $PBS_JOBID | cut -d'[' -f1)
-#fn_rm=$(ls /home/edrysdale/qsub | grep -v $jobid)
-#fn_keep=$(ls /home/edrysdale/qsub | grep $jobid)
-#
-#echo "jobid: "$jobid
-#echo "remove: "$fn_rm
-#for fn in $fn_rm; do
-#        rm /home/edrysdale/qsub/$fn
-#done
 
-# (2) Calculate the day/lead
-day=$(($PBS_ARRAYID))
-echo "--- day: "$day"------"
+# Assign the lead
+lead=$(($PBS_ARRAYID))
+model="gpy"  # should line up with mdls/{}.py
+echo "Lead: "$lead", model: "$model
 
-# Call model
-python run_mdl.py --day $day --lead $lead --nlags 10 --model $model
+# Call model: 212==July 30
+python -u run_gp.py --lead $ll --model gpy --dstart 0 --dend 212
 
-echo "##### end of script: "$day" ######"
+echo "##### end of script ######"
---
+++ pipeline_scp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# SCRIPT TRANSFER THE DATA PULLS TO HPF/SNOWQUEEN
+dir_pulls=../pulls
+
+is_mnt=$(pwd | cut -d'/' -f3)
+
+if [ $is_mnt == "c" ]; then
+  echo "we are on predator"
+elif [ $is_mnt == "erik" ]; then
+  echo "we are on snowqueen"
+elif [ $is_mnt == "edrysdale" ]; then
+  echo "we are on hpf"
+else
+  echo "where are we?!"
+  return
+fi
+
+if [ -d $dir_pulls ]; then
+  echo "pulls folder exists"
+  # Zip all the files
+  if [ $is_mnt == "c" ]; then
+    zip -r ../pulls.zip $dir_pulls
+    scp ../pulls.zip erik@snowqueen.sickkids.ca:/home/erik/Documents/projects/ED/master
+    scp ../pulls.zip edrysdale@data.ccm.sickkids.ca:/hpf/largeprojects/agoldenb/edrysdale/ED
+  fi
+  if [ $is_mnt == "erik" ]; then
+    unzip -o ../pulls.zip ../
+  fi
+  if [ $is_mnt == "edrysdale" ]; then
+    unzip -o ../pulls.zip ../
+  fi
+else
+  echo "pulls folder does not exist!"
+  return
+fi
+
+echo "-------- END OF SCRIPT ---------"
+
+
