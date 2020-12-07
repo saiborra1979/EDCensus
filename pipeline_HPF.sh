ef62c8d81b315a813fd9b462df3ccd0721d2539a
Erik Drysdale <erik.drysdale@sickkids.ca>
2020-12-07 16:00:25 -0500

    current GPU/HPF config

--- pipeline_HPF.sh
+++ pipeline_HPF.sh
@@ -34,9 +34,9 @@ echo "Lead: "$lead", model: "$model
 
 if [ "$groups" == "None" ]; then
 	echo "Not running a group"
-	python -u run_gp.py --lead $lead --model gpy --dtrain $ndays --dval 7 --dstart 60 --dend 303
+	python -u run_gp.py --lead $lead --model gpy --dtrain $ndays --dval 7 --dstart 60 --dend 335
 else
-	python -u run_gp.py --lead $lead --model gpy --dtrain $ndays --dval 7 --dstart 60 --dend 303 --groups $groups
+	python -u run_gp.py --lead $lead --model gpy --dtrain $ndays --dval 7 --dstart 60 --dend 335 --groups $groups
 fi
 
 echo "##### end of script ######"
