b727d81bd7d9646f2bdc6c8cc2154353016707be
Erik Drysdale <erik.drysdale@sickkids.ca>
2021-04-30 09:29:16 -0400

    gpu hpf

--- pipeline_GPU.sh
+++ pipeline_GPU.sh
@@ -5,10 +5,10 @@
 groups_final="mds arr CTAS"
 
 # LOOP OVER SAMPLE SIZE
-for ii in {1..4..1}; do
+for ii in {2..2..1}; do
 	t1=$((($ii-1)*6+1))
 	t2=$(($ii*6))
-for ndays in {3..3..1}; do
+for ndays in {1..7..1}; do
 	echo "number of training days: "$ndays", batch "$ii" of 4, array start="$t1", stop="$t2
 	qsub -N gpu_n_$ndays_batch_$t1 -v groups="$groups_final",ndays=$ndays -t $t1-$t2 pipeline_HPF.sh
 done
